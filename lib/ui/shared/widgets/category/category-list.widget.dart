fb9f5cfa28a9202fed8f35253c9bfa8dc481cdbd
Claylton dos Santos Pinto <clayltonsp@hotmail.com>
2021-05-01 14:03:59 -0300

    Criação da lista de categorias

---
+++ lib/ui/shared/widgets/category/category-list.widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:shopping/models/category-list-item.model.dart';
+
+class CategoryList extends StatelessWidget {
+  final List<CategoryListItemModel> categories;
+
+  CategoryList({@required this.categories});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
