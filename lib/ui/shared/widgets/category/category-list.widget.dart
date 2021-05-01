e2b6cacdb505d3b06795cd200644e6cb4429ab02
Claylton dos Santos Pinto <clayltonsp@hotmail.com>
2021-05-01 14:42:45 -0300

    Criando os estados do loader e lista de categorias

--- lib/ui/shared/widgets/category/category-list.widget.dart
+++ lib/ui/shared/widgets/category/category-list.widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:shopping/models/category-list-item.model.dart';
+import 'package:shopping/ui/shared/widgets/category/category-card.widget.dart';
+import 'package:shopping/ui/shared/widgets/shared/loader.widget.dart';
 
 class CategoryList extends StatelessWidget {
   final List<CategoryListItemModel> categories;
@@ -7,6 +9,27 @@ class CategoryList extends StatelessWidget {
   CategoryList({@required this.categories});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 90,
+      child: Loader(
+        object: categories,
+        callback: listCategories,
+      ),
+    );
+  }
+
+  Widget listCategories() {
+    return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          CategoryListItemModel item = categories[index];
+          return Padding(
+            padding: EdgeInsets.all(5),
+            child: CategoryCard(
+              item: item,
+            ),
+          );
+        });
   }
 }
