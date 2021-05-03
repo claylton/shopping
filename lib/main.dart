3891a2aea6b2d97e73a45b55aeb88b8dbe166b60
Claylton dos Santos Pinto <clayltonsp@hotmail.com>
2021-05-02 22:19:39 -0300

    Externalizando home

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping/blocs/home.bloc.dart';
-import 'package:shopping/ui/shared/widgets/category/category-list.widget.dart';
-import 'package:shopping/ui/shared/widgets/product/product-list.wigdet.dart';
+import 'package:shopping/ui/android/pages/home.page.dart';
 
 void main() => runApp(MyApp());
-
-/*class Main extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<HomeBloc>.value(
-          value: HomeBloc(),
-        )
-      ],
-    );
-  }
-}*/
 
 class MyApp extends StatelessWidget {
   @override
@@ -35,50 +21,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: HomePage(),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final HomeBloc bloc = Provider.of<HomeBloc>(context);
-
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            Text(
-              "Categorias",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CategoryList(
-              categories: bloc.categories,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Mais vendidos",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ProductList(
-              products: bloc.products,
-            ),
-          ],
-        ),
       ),
     );
   }
